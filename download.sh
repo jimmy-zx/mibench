c7ccb1ddf172a16b9dcbf5da0f713fe979eb4e6a
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2025-09-22 02:05:11 +0000

    add download.sh

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/env sh
+
+curl https://vhosts.eecs.umich.edu/mibench/automotive.tar.gz | tar xfz - automotive/susan
+curl https://vhosts.eecs.umich.edu/mibench/consumer.tar.gz | tar xfz - consumer/jpeg
